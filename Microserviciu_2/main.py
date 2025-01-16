4b90f8cbf899d2096353e5cd3abcecf803396f6a
Diaconu_Rares-George <125900360+spaxbo@users.noreply.github.com>
2025-01-16 11:06:23 -0800

    New update, finishing the backend

--- Microserviciu_2/main.py
+++ Microserviciu_2/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pymongo import MongoClient
 from bson import ObjectId
 from pydantic import BaseModel
 from schemas import Proba, Evaluare, Materiale
+from middleware import TokenValidationMiddleware
 
 client = MongoClient("mongodb://mongodb:27017")
 db = client.academia
@@ -13,12 +14,27 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(TokenValidationMiddleware)
+
+def validate_teacher_role(request: Request):
+    user = request.scope.get("user")
+    if user is None or user.get("role") != "teacher":
+        raise HTTPException(status_code=403, detail="Forbidden: insufficient permissions")
+
+def validate_get(request : Request):
+    user = request.scope.get("user")
+    if user is None or user.get("role") == "admin":
+        raise HTTPException(status_code=403, detail="Forbidden: insufficient permissions")
+
 def item_helper(item) -> dict : 
     item['_id'] = str(item['_id'])
     return item
 
 @app.post("/evaluare/", status_code=201)
-async def create_evaluation(evaluare : Evaluare) : 
+async def create_evaluation(evaluare : Evaluare, request: Request) : 
+
+    validate_teacher_role(request)
+    
     if sum(proba.pondere for proba in evaluare.probe) > 100 :
         raise HTTPException(status_code=400, detail="The sum of the weightings must be 100")
 
@@ -29,9 +45,9 @@ async def create_evaluation(evaluare : Evaluare) :
     return item_helper(db.evaluare.find_one({"_id" : result.inserted_id}))
 
 @app.post("/materiale/", status_code=201)
-async def create_materials(materiale : Materiale) : 
-    if materiale.material.pdf and materiale.material.structurat : 
-        raise HTTPException(status_code=422, detail="It can be introduced only pdf or structured schema, not both")
+async def create_materials(materiale : Materiale, request: Request) : 
+
+    validate_teacher_role(request)
 
     if db.materiale.find_one({"disciplina": materiale.disciplina, "tip_material": materiale.tip_material}):
         raise HTTPException(status_code=409, detail="The materials for this discipline already exists")
@@ -40,7 +56,10 @@ async def create_materials(materiale : Materiale) :
     return item_helper(db.materiale.find_one({"_id" : result.inserted_id}))
 
 @app.get("/evaluare/{disciplina}")
-async def get_evaluation(disciplina : str) :
+async def get_evaluation(disciplina : str, request: Request) :
+
+    validate_get(request)
+
     evaluare = db.evaluare.find_one({"disciplina" : disciplina})
     if not evaluare :
         raise HTTPException(status_code=404, detail="The evaluation for this lecture does not exist")
@@ -48,15 +67,20 @@ async def get_evaluation(disciplina : str) :
     return item_helper(evaluare)
 
 @app.get("/materiale/{disciplina}")
-async def get_materials(disciplina : str) :
-    materiale = db.materiale.find({"disciplina" : disciplina})
-    if not materiale :
+async def get_materials(disciplina : str, request: Request):
+
+    validate_get(request)
+    materials = list(db.materiale.find({"disciplina": disciplina}))
+    if not materials:
         raise HTTPException(status_code=404, detail="The materials for this lecture do not exist")
 
-    return [item_helper(m) for m in materiale]
+    return [item_helper(m) for m in materials]
 
 @app.put("/materiale/{disciplina}", status_code=200)
-async def update_materials(disciplina: str, updated_materiale: Materiale):
+async def update_materials(disciplina: str, updated_materiale: Materiale, request: Request):
+
+    validate_teacher_role(request)
+
     existing_material = db.materiale.find_one({"disciplina" : disciplina, "tip_material" : updated_materiale.tip_material})
     if not existing_material :
         raise HTTPException(status_code=404, detail="The materials for this lecture do not exist")
@@ -72,7 +96,10 @@ async def update_materials(disciplina: str, updated_materiale: Materiale):
     return item_helper(db.materiale.find_one({"disciplina": disciplina}))
 
 @app.put("/evaluare/{disciplina}", status_code=200)
-async def update_evaluation(disciplina: str, updated_evaluare: Evaluare):
+async def update_evaluation(disciplina: str, updated_evaluare: Evaluare, request: Request):
+
+    validate_teacher_role(request)
+
     existing_evaluare = db.evaluare.find_one({"disciplina": disciplina})
     if not existing_evaluare :
         raise HTTPException(status_code=404, detail="The evaluation for this lecture does not exist")
@@ -88,7 +115,10 @@ async def update_evaluation(disciplina: str, updated_evaluare: Evaluare):
     return item_helper(db.evaluare.find_one({"disciplina": disciplina}))
 
 @app.delete("/evaluare/{disciplina}", status_code=200)
-async def delete_evaluation(disciplina: str):
+async def delete_evaluation(disciplina: str, request: Request):
+
+    validate_teacher_role(request)
+
     existing_evaluare = db.evaluare.find_one({"disciplina": disciplina})
     if not existing_evaluare :
         raise HTTPException(status_code=404, detail="The evaluation for this lecture does not exist")
@@ -100,7 +130,10 @@ async def delete_evaluation(disciplina: str):
     }
 
 @app.delete("/materiale/{disciplina}", status_code=200)
-async def delete_materials(disciplina: str):
+async def delete_materials(disciplina: str, request: Request):
+
+    validate_teacher_role(request)
+
     existing_material = db.materiale.find_one({"disciplina": disciplina})
     if not existing_material :
         raise HTTPException(status_code=404, detail="The materials for this lecture do not exist")
